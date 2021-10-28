ac208d3b79cfabb55ba063a520a12f9ca16c577e
Dynee <lucaspadden@gmail.com>
2021-10-28 17:00:42 -0400

    commititng everything before break, added a JoinTemplate class to handle joining multiple ddl's together

---
+++ pygendata/templates/join.py
@@ -0,0 +1,39 @@
+from pygendata.managers import CSVManager
+import logging
+import os
+from pygendata.ddl import DDL
+
+class JoinTemplate:
+    def __init__(self):
+        self.tables = []
+    
+    def parse(self, file):
+        manager = CSVManager()
+        try:
+            statement = manager.read(file)
+            keys, *tables = statement.split('/')
+            keys = keys.strip('\n')
+            keys = keys.split(',')
+            tables = [x.strip() for x in tables]
+            ddls = [DDL(stmt) for stmt in tables]
+            for ddl in ddls:
+                ddl.get_columns()
+                ddl.columns = [column.strip() for column in ddl.columns]
+                ddl.create_headers()
+            self.tables = ddls
+        except IOError as e:
+            logging.warning(str(e))
+    
+    # TODO: Auto increment rows, make sure data is synchronized between joined tables
+    def scaffold(self, rows):
+        t1_rows = []
+        if self.tables:
+            first = self.tables[0]
+            for i in range(rows):
+                t1_rows.append(first.create_row(i))
+        print(t1_rows)
+
+jt = JoinTemplate()
+filepath = os.getcwd()
+jt.parse(f"{filepath}/aog.sql")
+jt.scaffold(10)
